82989a2775e201c9b288f3135569705936b4a40e
ALMardasDEV <albusaidi9094@gmail.com>
2025-04-01 18:55:59 +0400

    new fix

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'package:project/pages/SecondPage.dart';
 void main() {
   runApp(MyApp());
 }
-
+//fix
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
